0c2d303461e65f618a730298a56ea974938b29d4
barodzich aliaksandr <barodzichaliaksandr@gmail.com>
2024-12-13 19:10:05 +0300

    feat: implement countVowels fn

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -311,8 +311,16 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  let count = 0;
+  const vowels = 'aeouiy';
+  const strLowerCase = str.toLowerCase();
+
+  for (let i = 0; i < strLowerCase.length; i += 1) {
+    if (vowels.includes(strLowerCase[i])) count += 1;
+  }
+
+  return count;
 }
 
 /**
